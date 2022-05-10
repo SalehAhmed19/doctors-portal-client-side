f339328c72abb052e02ad68c8779ac49494c697b
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-05-10 13:34:00 +0600

    initial setup complete

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./Pages/Shared/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
